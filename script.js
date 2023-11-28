a50888f4dcd310703459396d1029614336c97538
Maschio Pablo <134315465+MaschioPablo@users.noreply.github.com>
2023-11-28 18:13:25 -0300

    Add files via upload
    commit pablo, falta el mov

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function mostrarProductos() {
     .join("");
 }
 
-document.getElementById(`botonAccion`).addEventListener("click", () => {
+document.getElementById("botonAccion").addEventListener("click", () => {
   let codigo = document.getElementById("codigo").value;
   if (codigo.match(/^(new|del|mov)\.\d{4}[A-D]$/)) {
     const almacenProducto = codigo.charAt(codigo.length - 1);
@@ -48,37 +48,65 @@ document.getElementById(`botonAccion`).addEventListener("click", () => {
         mostrarProductos();
       }
     } else if (codigo.match(/^del\.\d{4}[A-D]$/)) {
-      if (productos.almacenProducto.includes(codigo)) {
-        productos.almacenProducto = productos.almacenProducto.filter(
-          (producto) => producto !== codigo
-        );
-        mostrarProductos();
-      } else {
-        mensajeGenerico(
-          "Código erroneo",
-          "El producto no se encuentra en el depósito",
-          "info"
-        );
+      const numeros = codigo.match(/\d+/);
+      if (numeros && numeros.length > 0) {
+        const producto = numeros[0].toString();
+        if (productos[almacenProducto].includes(producto)) {
+          productos[almacenProducto] = productos[almacenProducto].filter(
+            (p) => p !== producto
+          );
+          mostrarProductos();
+          mensajeProductoEliminado(producto, almacenProducto);
+        } else {
+          mensajeGenerico(
+            "Código Erroneo",
+            "El producto no se encuentra en el depósito",
+            "info"
+          );
+        }
       }
     } else if (codigo.match(/^mov\.\d{4}[A-D]$/)) {
-      alert("mov");
-      if (productos["A"].includes(numeros[0])) {
+      const numeros = codigo.match(/\d+/g);
+      if (numeros && numeros.length > 0) {
+        const producto = numeros[0].toString();
 
-      } else if (productos["B"].includes(numeros[0])) {
-          
-      } else if(productos["C"].includes(numeros[0])){
-          
+        // Verificar si el producto existe en el almacén actual
+        if (productos[almacenProducto].includes(producto)) {
+
+          // Verificar a qué almacén se está moviendo el producto
+          const nuevoAlmacen = codigo.charAt(codigo.length - 1);
+
+          // Evitar duplicados en el nuevo almacén
+          if (!productos[nuevoAlmacen].includes(producto)) {
+            // Mover el producto al nuevo almacén
+            productos[almacenProducto] = productos[almacenProducto].filter(
+              (p) => p !== producto
+            );
+            productos[nuevoAlmacen].push(producto);
+            mostrarProductos();
+          }
+        }
+      } else if (codigo === "") {
+        mensajeProductoVacio();
       } else {
-
+        mensajeProductoErroneo();
       }
-    }else if (codigo == "") {
-    mensajeProductoVacio();
-  } else {
-    mensajeProductoErroneo();
+    }
   }
-}});
+});
 
 //----------------------Funciones para mensajes----------------------//
+
+function mensajeProductoEliminado(producto, deposito) {
+  Swal.fire({
+    icon: "success",
+    title: `Producto ${producto} eliminado de ${deposito}`,
+    showConfirmButton: false,
+    timer: 1500,
+  });
+  let snd = new Audio("./snd/eliminar producto.mp3");
+  snd.play();
+}
 
 function mensajeGenerico(mensaje, mensajeDeposito, tipo) {
   Swal.fire(mensaje, mensajeDeposito, tipo);
